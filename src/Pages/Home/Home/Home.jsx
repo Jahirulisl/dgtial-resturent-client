fa52ef645793cc9c344babeaa64faf868096cea0
Jahirul Islam <nockjahirul@gmail.com>
2025-08-21 08:17:02 +0600

    add templet section and som bolg added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,14 @@
 import Banner from "../Banner/Banner";
 import Catagory from "../Catagory/Catagory";
+import Templet from "../Templet/Templet";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Catagory></Catagory>
+      <Templet></Templet>
+      
     </div>
   );
 };
---
+++ src/Pages/Home/Templet/Templet.jsx
@@ -0,0 +1,20 @@
+import bgImg from '../../../assets/home/chef-service.jpg';
+const Templet = () => {
+  return (
+    <div className=" flex justify-center h-screen items-center mb-24"
+      style={{ backgroundImage: `url(${bgImg})` }}
+
+    >
+
+      <div className="bg-base-300 w-4/6 p-8 m-4 ">
+        <h2 className=" text-3xl text-center uppercase">Bistro boss</h2>
+        <p>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed natus facere tempora perferendis aspernatur quidem! Laudantium vero at tenetur repellat illo. Necessitatibus eveniet quam similique pariatur quia esse architecto nulla.
+       </p>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Templet;
