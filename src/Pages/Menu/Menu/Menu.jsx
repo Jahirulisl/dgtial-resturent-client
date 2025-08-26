8014c7383449807b1b95cb3fb434d56cbf5fd40a
Jahirul Islam <nockjahirul@gmail.com>
2025-08-26 11:15:55 +0600

    add some blog cover and munu and others

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,11 +1,17 @@
+// for helmet start
 import { Helmet} from 'react-helmet-async';
+import coverImg from "../../../assets/menu/banner3.jpg";
+import Cover from '../../Home/Home/Shared/Cover/Cover';
+import PopulerMenu from '../../Home/PopulerMenu/PopulerMenu';
 
 const Menu = () => {
   return (
     <div>
-       <Helmet>
-        <title>Bistro Boss | menu</title>
+      <Helmet>
+        <title> Eduction | Munu</title>
       </Helmet>
+      <Cover img ={coverImg} title="Our Munu"></Cover>
+      <PopulerMenu></PopulerMenu>
     </div>
   );
 };
