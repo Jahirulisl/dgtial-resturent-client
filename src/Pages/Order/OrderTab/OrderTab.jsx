4986cdbe2f81c801bda966c58670fd239ba39557
Jahirul Islam <nockjahirul@gmail.com>
2025-09-06 19:10:11 +0600

    add pagination in orderTab and some blog

--- src/Pages/Order/OrderTab/OrderTab.jsx
+++ src/Pages/Order/OrderTab/OrderTab.jsx
@@ -1,12 +1,40 @@
 import FoodCard from "../../../Components/SectionTitle/FoodCard/FoodCard";
+//FOR PAGNITION START
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+//FOR PAGNITION END
 
-const OrderTab = ({items}) => {
+const OrderTab = ({ items }) => {
+
+  //MAKE PAGINATION 
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  };
+
   return (
-    <div className="grid md:grid-cols-3 gap-10">
-      {
-        items.map(item => <FoodCard key={item._id}
-          item={item}></FoodCard>)
-      }
+    <div>
+
+      {/* FROM PAGNATION CUSTOM  */}
+      <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="grid md:grid-cols-3 gap-10">
+            {
+              items.map(item => <FoodCard key={item._id}
+                item={item}></FoodCard>)
+            }
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
