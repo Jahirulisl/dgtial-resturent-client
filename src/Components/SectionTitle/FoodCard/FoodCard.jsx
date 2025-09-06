f7aa1c098b333418a4d227b4c4c82bd5902e2349
Jahirul Islam <nockjahirul@gmail.com>
2025-09-06 16:33:00 +0600

    some blog add foodCard

--- src/Components/SectionTitle/FoodCard/FoodCard.jsx
+++ src/Components/SectionTitle/FoodCard/FoodCard.jsx
@@ -4,18 +4,18 @@ const FoodCard = ({item}) => {
   const {name,image,price,recipe} = item;
   return (
     //card from daisyui khala start
-    <div className="card bg-base-100 w-96 shadow-sm">
+    <div className="card bg-base-100 w-96 shadow-xl ">
       <figure>
         <img
           src={image}
           alt="Shoes" />
       </figure>
       <p className="absolute right-0  mr-4  rounded mt-4 px-4 bg-slate-900 text-white ">${price}</p>
-      <div className="card-body">
+      <div className="card-body flex flex-col items-center ">
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Add to card</button>
+          <button className="btn btn-outline border-0 border-orange-400 bg-slate-100 border-b-4 mt-4">Add to card</button>
         </div>
       </div>
     </div>
