cfcd6c7a743a17747a2a190c6db6233552599d88
Jahirul Islam <nockjahirul@gmail.com>
2025-09-22 21:49:08 +0600

    add user checking information use some blog socialLogin

--- src/Components/SocialLogin/SocialLogin.jsx
+++ src/Components/SocialLogin/SocialLogin.jsx
@@ -1,13 +1,32 @@
 import { FaGoogle } from "react-icons/fa";
 import useAuth from "../../hooks/useAuth";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
+  //for user checking step 1 start
+  const axiosPublic = useAxiosPublic();
+  //for user checking step 1 end
+  //for navigate in user chicking step 3 sta
+  const navigate = useNavigate();
+  //for navigate in user chicking step 3 end
   const {googleSignIn} = useAuth();
   //for on click google button
   const handleGoogleSignIn = () =>{
     googleSignIn()
     .then(result =>{
       console.log(result.user);
+      //for user checking step 2 start
+     const userInfo ={
+      email:result.user?.email,
+      name: result.user?.displayName
+     }
+     axiosPublic.post('users',userInfo)
+     .then(res =>{
+      console.log(res.data);
+      navigate('/')
+     })
+      //for user checking step 2 end
     })
   }
   return (
